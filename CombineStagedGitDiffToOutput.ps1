9567943252f6bc4f689fd4837c8f10a43e14a736
German Fica <germafica@gmail.com>
2023-09-07 12:44:48 -0300

    Fix PowerShell script to output staged git diffs to text file
    - Add PowerShell script to handle git diffs.
    - The script first checks and deletes an existing 'output.txt' file if it exists.
    - It creates a StreamWriter instance with UTF-8 encoding without BOM.
    - Executes `git diff --staged` and processes the output line by line.
    - Adds separators and writes these lines into 'output.txt'.
    
    This script is useful for capturing staged git diffs into a human-readable text file for documentation or auditing purposes.

--- CombineStagedGitDiffToOutput.ps1
+++ CombineStagedGitDiffToOutput.ps1
@@ -7,18 +7,34 @@ if (Test-Path output.txt) {
 $utf8WithoutBom = New-Object System.Text.UTF8Encoding $false
 $stream = New-Object System.IO.StreamWriter "output.txt", $false, $utf8WithoutBom
 
-# Ejecutar git diff --staged y guardar la salida
-$diffOutput = & git diff --staged . 2>&1
-
-# Escribir la salida en output.txt
+# Escribir el encabezado en output.txt
 $stream.WriteLine("Git Diff --Staged Output:")
-$stream.WriteLine("``````")
 
-# Escribir cada línea de la salida del comando en el archivo
-$diffOutput | ForEach-Object {
+# Variable para controlar si estamos en el primer archivo
+$firstFile = $true
+
+# Ejecutar git diff --staged y procesar la salida línea por línea
+& git diff --staged . 2>&1 | ForEach-Object {
+    # Si la línea comienza con "diff --git", es el inicio de un nuevo archivo
+    if ($_ -match "^diff --git") {
+        # Si no es el primer archivo, añadir separador y salto de línea
+        if (-not $firstFile) {
+            $stream.WriteLine("``````")
+            $stream.WriteLine("")
+        }
+
+        # Añadir un separador inicial antes del nuevo archivo
+        $stream.WriteLine("``````")
+
+        # No es el primer archivo anymore
+        $firstFile = $false
+    }
+
+    # Escribir la línea actual al archivo
     $stream.WriteLine($_)
 }
 
+# Añadir un separador final después del último archivo
 $stream.WriteLine("``````")
 
 # Cerrar el StreamWriter al final
