dfe5ebff7d67795ce61f50dcf29b5ff2c0fa8606
German Fica <germafica@gmail.com>
2023-09-07 12:38:27 -0300

    Fix CombineStagedGitDiffToOutput.ps1

--- CombineStagedGitDiffToOutput.ps1
+++ CombineStagedGitDiffToOutput.ps1
@@ -5,15 +5,20 @@ if (Test-Path output.txt) {
 
 # Crear una instancia StreamWriter con UTF8 sin BOM
 $utf8WithoutBom = New-Object System.Text.UTF8Encoding $false
-$stream = New-Object System.IO.StreamWriter "output.txt", $true, $utf8WithoutBom
+$stream = New-Object System.IO.StreamWriter "output.txt", $false, $utf8WithoutBom
 
 # Ejecutar git diff --staged y guardar la salida
-$diffOutput = & git diff --staged .
+$diffOutput = & git diff --staged . 2>&1
 
 # Escribir la salida en output.txt
 $stream.WriteLine("Git Diff --Staged Output:")
 $stream.WriteLine("``````")
-$stream.WriteLine($diffOutput)
+
+# Escribir cada línea de la salida del comando en el archivo
+$diffOutput | ForEach-Object {
+    $stream.WriteLine($_)
+}
+
 $stream.WriteLine("``````")
 
 # Cerrar el StreamWriter al final
