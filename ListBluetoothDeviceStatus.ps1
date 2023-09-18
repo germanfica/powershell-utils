11f94663222c3ee016a47ad5cbfdb893b05a2a47
German Fica <germafica@gmail.com>
2023-09-17 23:15:38 -0300

    Add PowerShell script for Bluetooth device categorization
    This commit adds a new PowerShell script that categorizes Bluetooth devices based on their status and connectivity.
    
    Features:
    - Fetches all PnP devices using Get-PnpDevice.
    - Initializes arrays to store different types of devices (paired, failed, connected, and disconnected).
    - Categorizes Bluetooth devices into these arrays based on their statuses and properties.
    - Outputs the categorized device information to the console.
    
    This new script provides a more organized way to manage and view the status of Bluetooth devices on a system.

---
+++ ListBluetoothDeviceStatus.ps1
@@ -0,0 +1,47 @@
+# Obtener todos los dispositivos PnP
+$allDevices = Get-PnpDevice
+
+# Inicializar arreglos vacíos para almacenar diferentes tipos de dispositivos
+$pairedDevices = @()
+$failedDevices = @()
+$connectedDevices = @()
+$disconnectedDevices = @()
+
+# Filtrar y categorizar los dispositivos Bluetooth
+foreach ($device in $allDevices) {
+    if ($device.Class -eq "Bluetooth" -or $device.FriendlyName -like "*Bluetooth*") {
+        $deviceInfo = New-Object PSObject -Property @{
+            "DeviceID"     = $device.DeviceID
+            "Name"         = $device.FriendlyName
+            "Status"       = $device.Status
+            "Manufacturer" = $device.Manufacturer
+        }
+
+        # Categorizar el dispositivo según su estado
+        switch ($device.Status) {
+            "OK" { $pairedDevices += $deviceInfo }
+            "Error" { $failedDevices += $deviceInfo }
+        }
+
+        # Determinar si el dispositivo está conectado o no
+        $deviceStatus = Get-PnpDeviceProperty -InstanceId $device.DeviceID -KeyName "DEVPKEY_Device_DevNodeStatus" | Select-Object -ExpandProperty Data
+        if ($deviceStatus -eq 0x1000a0) {
+            $connectedDevices += $deviceInfo
+        } else {
+            $disconnectedDevices += $deviceInfo
+        }
+    }
+}
+
+# Mostrar la información
+Write-Host "Dispositivos emparejados:"
+$pairedDevices | Format-Table -Property "DeviceID", "Name", "Status", "Manufacturer"
+
+Write-Host "`nDispositivos que fallaron:"
+$failedDevices | Format-Table -Property "DeviceID", "Name", "Status", "Manufacturer"
+
+Write-Host "`nDispositivos conectados:"
+$connectedDevices | Format-Table -Property "DeviceID", "Name", "Status", "Manufacturer"
+
+Write-Host "`nDispositivos no conectados:"
+$disconnectedDevices | Format-Table -Property "DeviceID", "Name", "Status", "Manufacturer"
